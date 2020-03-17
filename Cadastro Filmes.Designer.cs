45539165b37da96f04d9a82c40fb003842fc5e8f
gregprog2020 <61755648+gregprog2020@users.noreply.github.com>
2020-03-17 17:24:42 -0300

    Add files via upload

---
+++ Cadastro Filmes.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Locadora_1._2
+{
+    partial class frmCadastroFilmes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.txbID = new System.Windows.Forms.TextBox();
+            this.txbTitulo = new System.Windows.Forms.TextBox();
+            this.txbGenero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.grid1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.AutoSize = true;
+            this.btnGravar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGravar.Location = new System.Drawing.Point(12, 280);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(75, 30);
+            this.btnGravar.TabIndex = 0;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.AutoSize = true;
+            this.btnExcluir.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(93, 280);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 30);
+            this.btnExcluir.TabIndex = 1;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // txbID
+            // 
+            this.txbID.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbID.Location = new System.Drawing.Point(75, 21);
+            this.txbID.Name = "txbID";
+            this.txbID.ReadOnly = true;
+            this.txbID.Size = new System.Drawing.Size(43, 26);
+            this.txbID.TabIndex = 2;
+            // 
+            // txbTitulo
+            // 
+            this.txbTitulo.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTitulo.Location = new System.Drawing.Point(75, 53);
+            this.txbTitulo.MaxLength = 100;
+            this.txbTitulo.Name = "txbTitulo";
+            this.txbTitulo.Size = new System.Drawing.Size(169, 26);
+            this.txbTitulo.TabIndex = 3;
+            // 
+            // txbGenero
+            // 
+            this.txbGenero.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbGenero.Location = new System.Drawing.Point(75, 85);
+            this.txbGenero.Name = "txbGenero";
+            this.txbGenero.Size = new System.Drawing.Size(113, 26);
+            this.txbGenero.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ID :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Titulo :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Genero :";
+            // 
+            // grid1
+            // 
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.Location = new System.Drawing.Point(12, 124);
+            this.grid1.Name = "grid1";
+            this.grid1.Size = new System.Drawing.Size(370, 150);
+            this.grid1.TabIndex = 8;
+            this.grid1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // frmCadastroFilmes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(427, 341);
+            this.Controls.Add(this.grid1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbGenero);
+            this.Controls.Add(this.txbTitulo);
+            this.Controls.Add(this.txbID);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnGravar);
+            this.Name = "frmCadastroFilmes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro Filmes";
+            this.Load += new System.EventHandler(this.frmCadastroFilmes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.TextBox txbID;
+        private System.Windows.Forms.TextBox txbTitulo;
+        private System.Windows.Forms.TextBox txbGenero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView grid1;
+    }
+}
---
+++ Cadastro Filmes.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Locadora_1._2.Code.BLL;
+using Locadora_1._2.Code.DTO;
+
+namespace Locadora_1._2
+{
+    public partial class frmCadastroFilmes : Form
+    {
+        CadastroFilmeBLL bll = new CadastroFilmeBLL();
+        CadastroFilmeDTO dto = new CadastroFilmeDTO();
+        public frmCadastroFilmes()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCadastroFilmes_Load(object sender, EventArgs e)
+        {
+            CarregarGrid();
+        }
+
+        private void CarregarGrid()
+        {
+            grid1.DataSource = bll.SelecionaTodosFilmes();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txbID.Text = grid1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txbTitulo.Text = grid1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txbGenero.Text = grid1.Rows[e.RowIndex].Cells[2].Value.ToString();
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+
+            dto.Titulo = txbTitulo.Text;
+            dto.Genero = txbGenero.Text;
+
+            if (txbID.Text == "")
+            {
+                bll.Inserir(dto);
+                MessageBox.Show(" Cadastrado com Sucesso!", "Inserido com Sucesso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                dto.Id = int.Parse(txbID.Text);
+                bll.Atualizar(dto);
+                MessageBox.Show(" Atualizado com Sucesso!", "Atualização com Sucesso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            LimparTela();
+            CarregarGrid();
+        }
+        private void LimparTela()
+        {
+            txbID.Clear();
+            txbTitulo.Clear();
+            txbGenero.Clear();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (txbID.Text != "")
+            {
+
+                var result = MessageBox.Show("Deseja realmente excluir?", "Exclusão Filme", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    bll.Excluir(txbID.Text);
+                    MessageBox.Show(" Excluido com Sucesso!", "Exclusão com Sucesso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LimparTela();
+                    CarregarGrid();
+                }
+            }
+
+        }
+    }
+
+    
+}
