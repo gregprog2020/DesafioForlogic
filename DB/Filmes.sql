400c34fc9701a843bea93f3bc13c476cec535646
gregprog2020 <61755648+gregprog2020@users.noreply.github.com>
2020-03-18 02:40:50 -0300

    Add files via upload

---
+++ DB/Filmes.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id_Filme] INT NOT NULL PRIMARY KEY, 
+    [Titulo] VARCHAR(50) NOT NULL, 
+    [Genero] VARCHAR(50) NOT NULL
+)
---
+++ DB/clientes.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id_Cliente] INT NOT NULL , 
+    [Nome] VARCHAR(100) NOT NULL, 
+    [Telefone] VARCHAR(50) NULL, 
+    [Cpf] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([Id_Cliente])
+)
